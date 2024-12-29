a186814650dc8508933b71d05c3641979d1ca615
SilianZ <silian_zheng@outlook.com>
2024-12-29 13:50:25 +0800

    fix: 修复漏洞

--- core/router.py
+++ core/router.py
@@ -55,7 +55,6 @@ class Router:
                     if isinstance(storage, AListStorage):
                         url = await storage.measure(size)
                         response = web.HTTPFound(url)
-                        await response.prepare(request)                        
                         return response
 
                 buffer = b"\x00\x66\xcc\xff" * 256 * 1024
